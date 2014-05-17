55e4e02a1f1be48e7582f810b63e923d8acc71f8
dora56 <doradoora56@gmail.com>
2014-05-18 01:03:41 +0900

    Userクラスのテストコードを作成

---
+++ Test/UserTest.php
@@ -0,0 +1,20 @@
+<?PHP
+//テスト対象のクラス定義を読み込み
+require_once __DIR__ .'/../lib/User.php';
+
+class UserTest extends PHPUnit_Framework_TestCase {
+
+    public function testConstruct() {
+	
+	    $user =new User('佐藤');
+		$this->assertEquals('佐藤', $user->name);
+	}
+	
+	public function testCreateMassage() {
+	
+	    $user = User('河合');
+		$test = $user->createMassage();
+		$expected = 'こんにちは河合さん';
+		$this->assertEquals($expected,$test);
+	}
+}
